899a39d122689f2c54a4b6051bca12c8d18d0e0d
Raed Asdi <raedasdi@gmail.com>
2019-11-05 13:13:43 -0600

    deck.cpp V1.0.0
    ~wrote some functions the Deck class
    ~NOT COMPLETE

--- Go Fish!/deck.cpp
+++ Go Fish!/deck.cpp
@@ -1,13 +1,34 @@
 #include "deck.h"
 #include "card.h"
+#include <ctime>
 
 using namespace std;   
     
     Deck::Deck(){           // pristine, sorted deck
-
+        myIndex = 0;
+        for (int i = 0; i < 4; i++){
+            for (int j = 1; j < 14; j++){
+                myCards[myIndex] = Card c(j, i);
+                myIndex++;
+            }
+        }
+        myIndex = 0;
     }
 
     void Deck::shuffle(){   // shuffle the deck, all 52 cards present
+        srand((unsigned)time(0));
+
+        int sw1, sw2;
+
+
+        sw1 = rand() % 52;
+        sw2 = rand() % 52;
+
+        while (sw1 == sw2) {
+            sw1 = rand() % 52;
+        }
+        
+
 
     }
 
