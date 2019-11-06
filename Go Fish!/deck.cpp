d2dc189e8054a2839e5024de5d212d0cb817231e
Raed Asdi <raedasdi@gmail.com>
2019-11-05 20:55:23 -0600

    deck.cpp Update V1.1.0
    ~finished shuffle, deal, and size methods

--- Go Fish!/deck.cpp
+++ Go Fish!/deck.cpp
@@ -12,30 +12,43 @@ using namespace std;
                 myIndex++;
             }
         }
+
         myIndex = 0;
+
+        time_t current_time;
+        current_time = time(NULL);
+        srand(current_time);
     }
 
     void Deck::shuffle(){   // shuffle the deck, all 52 cards present
-        srand((unsigned)time(0));
+        unsigned int sw1=1, sw2=1;
+        Card temp;
 
-        int sw1, sw2;
+        for (int i = 0, i < SIZE*2; i++) {
 
+            while (sw1 < myIndex || sw2 < myIndex) {
+                sw1 = rand() % 52;
+                sw2 = rand() % 52;
+            }
+            temp = myCards[sw1];
+            myCards[sw1] = myCards[sw2];
+            myCards[sw2] = temp;
 
-        sw1 = rand() % 52;
-        sw2 = rand() % 52;
-
-        while (sw1 == sw2) {
-            sw1 = rand() % 52;
         }
-        
+
 
 
     }
 
     Card Deck::dealCard(){   // get a card, after 52 are dealt, fail 
-
+        if(myIndex<52) {
+            myIndex++;
+            return myCards[myIndex-1];
+        }
+        else
+            cout<<"No More Cards!"<<endl;
     }
 
     int  Deck::size() const{ // # cards left in the deck
-
+        return (SIZE-(myIndex+1));
     }
