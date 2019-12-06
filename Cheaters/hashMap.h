8f63f7362e2af9c9edd7e1819be1ca90f373d895
s-usmani <32533622+s-usmani@users.noreply.github.com>
2019-12-06 12:35:43 -0600

    working hashMap

--- Cheaters/hashMap.h
+++ Cheaters/hashMap.h
@@ -33,14 +33,48 @@ public:
          hashMap.push_back(temp);
       }
    }
+
+   vector<HashNode *> getMap(){
+      return hashMap;
+   }
+
+   void print(){
+      for (int i = 0; i < 10000000; i++){
+         if (hashMap.at(i) != NULL){
+            // cout << "0" << endl;
+            HashNode *temp = hashMap.at(i);
+            while (temp != NULL){
+               cout << temp->index << endl;
+               temp = temp->next;
+            }
+         }
+      }
+   }
    void push(string const &s, int fileIndex){
-      int key = 0;
+      long key = 0;
       for (int i = 0; i < s.length(); i++){
          char character = s[s.length()-i-1];
          int ascii = int(character);
-         key += ascii*pow(27, i);
+         // cout << ascii << endl;
+         long add = (long) abs((ascii*pow(7, i))) % 10000000;
+
+         key = (key+add)  % 10000000 ;
+         // cout << key << endl;
       }
-      hashMap.at(key)->index = fileIndex;
+      if (hashMap.at(key) == NULL){
+         HashNode *temp = new HashNode;
+         temp->index = fileIndex;
+         temp->next = NULL;
+         hashMap.at(key) = temp;
+         // hashMap.at(key)->index = fileIndex;
+         // hashMap.at(key)->next = NULL;
+      }
+      else{
+         HashNode *temp = new HashNode;
+         temp->index = fileIndex;
+         temp->next = hashMap.at(key);
+         hashMap.at(key) = temp;
+      }
    }
 
 };
