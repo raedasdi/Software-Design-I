535ec2d034535cca73fef29cd19f0e615b21683d
s-usmani <32533622+s-usmani@users.noreply.github.com>
2019-12-05 20:40:06 -0600

    Update hashMap.h

--- Cheaters/hashMap.h
+++ Cheaters/hashMap.h
@@ -11,15 +11,37 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 class HashMap{
+
 private:
+   struct HashNode {
+      int index;
+      HashNode *next;
+   };
+   vector<HashNode *> hashMap;
+
+
 
 public:
-
-
+   HashMap(){
+      for (int i = 0; i < 10000000; i++){
+         HashNode *temp = NULL;
+         hashMap.push_back(temp);
+      }
+   }
+   void push(string const &s, int fileIndex){
+      int key = 0;
+      for (int i = 0; i < s.length(); i++){
+         char character = s[s.length()-i-1];
+         int ascii = int(character);
+         key += ascii*pow(27, i);
+      }
+      hashMap.at(key)->index = fileIndex;
+   }
 
 };
 #endif
