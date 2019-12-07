ccd5306a84385679eab28aa937ce20c28990604c
s-usmani <32533622+s-usmani@users.noreply.github.com>
2019-12-06 18:44:09 -0600

    Update hashMap.h

--- Cheaters/hashMap.h
+++ Cheaters/hashMap.h
@@ -22,27 +22,73 @@ private:
       int index;
       HashNode *next;
    };
-   vector<HashNode *> hashMap;
+
 
 
 
 public:
+   vector<HashNode *> hashMap;
    HashMap(){
-      for (int i = 0; i < 10000000; i++){
+      for (int i = 0; i < 1000003; i++){
          HashNode *temp = NULL;
          hashMap.push_back(temp);
       }
    }
 
+   vector<HashNode *> getMap(){
+      return hashMap;
+   }
+
+   void print(){
+      for (int i = 0; i < 1000003; i++){
+         if (hashMap.at(i) != NULL){
+            // cout << "0" << endl;
+            HashNode *temp = hashMap.at(i);
+            while (temp != NULL){
+               cout << temp->index << endl;
+               temp = temp->next;
+            }
+         }
+      }
+   }
    void push(string const &s, int fileIndex){
-      int key = 0;
+      long key = 0;
       for (int i = 0; i < s.length(); i++){
          char character = s[s.length()-i-1];
          int ascii = int(character);
-         key += ascii*pow(27, i);
+         // cout << ascii << endl;
+         long add = (long) abs((ascii*pow(7, i))) % 1000003;
+
+         key = (key+add)  % 1000003 ;
+         // cout << key << endl;
       }
-      hashMap.at(key)->index = fileIndex;
+      if (hashMap.at(key) == NULL){
+         HashNode *temp = new HashNode;
+         temp->index = fileIndex;
+         temp->next = NULL;
+         hashMap.at(key) = temp;
+         // hashMap.at(key)->index = fileIndex;
+         // hashMap.at(key)->next = NULL;
+      }
+      else{
+         HashNode *temp = new HashNode;
+         temp->index = fileIndex;
+         temp->next = hashMap.at(key);
+         hashMap.at(key) = temp;
+      }
    }
+
+   void list2vector(int key, vector<int> &fileIndices){
+      HashNode *temp = hashMap.at(key);
+      while (temp != NULL){
+         // cout << temp->index << endl;
+         fileIndices.push_back(temp->index);
+         temp = temp->next;
+      }
+
+   }
+
+
 
 };
 #endif
