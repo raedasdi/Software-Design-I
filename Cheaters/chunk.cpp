8fa4ae5f2536828f0f207c9f1ef1c16e61272169
s-usmani <32533622+s-usmani@users.noreply.github.com>
2019-12-06 18:44:57 -0600

    Update chunk.cpp

--- Cheaters/chunk.cpp
+++ Cheaters/chunk.cpp
@@ -9,10 +9,12 @@ using namespace std;
 
 Chunk::Chunk() {
     chunkSize = 6;
+    index = 0;
 }
 
 Chunk::Chunk(int n){
     chunkSize = n;
+    index = 0;
 }
 
 int Chunk::getChunkSize(){
@@ -20,14 +22,34 @@ int Chunk::getChunkSize(){
 }
 
 void Chunk::fillDocString(string path) {
-    ifstream file;
-    file.open(path);
+    fstream file;
+    index = 0;
+    // cout << path << endl;
+    file.open(path.c_str());
     string temp;
+    string push;
+    while (file >> temp)
+    {
+         int j = 0;
+         for (int i = 0; i < temp.length(); i++){
 
-    while (!file.eof()){
-        file >> temp;
-        chunkList.push_back(temp);
+            if ((temp[i] <= 90 && temp[i] >= 65) || (temp[i] <= 122 && temp[i] >= 97)){
+               push[j] = temp[i];
+               j++;
+            }
+         }
+         for (int i = 0; i < push.length(); i++)
+         {
+            if (push[i] <= 90 && push[i] >= 65){
+               push[i] += 32;
+            }
+         }
+         docstring.push_back(push);
+
     }
+
+    //cout << docstring.size();
+    // file.close();
 }
 
 void Chunk::makeChunkList() {
@@ -35,13 +57,13 @@ void Chunk::makeChunkList() {
     int j;
 
 
-    while (docstring.size() > 0) {
+    while (docstring.size() > this->chunkSize) {
         j = 0;
         while(j < this->chunkSize){
             temp = docstring.at(j);
-            for (string::iterator i = temp.begin(); i < temp.end(); i++){
-                if(!isalnum(*i)) temp.erase(i);
-            }
+            // for (string::iterator i = temp.begin(); i < temp.end(); i++){
+            //     if(!isalnum(*i)) temp.erase(i);
+            // }
 
             chunk+=temp;
             j++;
@@ -52,7 +74,8 @@ void Chunk::makeChunkList() {
 }
 
 string Chunk::getChunk() {
-    string chunk = chunkList.at(0);
-    chunkList.erase(chunkList.begin());
+    string chunk = chunkList[index];
+    index++;
+
     return chunk;
 }
